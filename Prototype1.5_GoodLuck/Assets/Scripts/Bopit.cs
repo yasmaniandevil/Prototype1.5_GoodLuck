a7c76ed3b292cafef64b07588bd87ff61f14f0aa
yasmaniandevil <44387512+yasmaniandevil@users.noreply.github.com>
2023-09-07 15:15:46 -0400

    trying to figure out simon says

---
+++ Prototype1.5_GoodLuck/Assets/Scripts/Bopit.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bopit : MonoBehaviour
+{
+
+    private AudioSource _audioSource;
+    bool isPlaying = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        _audioSource.Play();
+        
+    }
+    
+    
+    //call audio source function play
+        
+    //im the box that got clicked check thru the list
+        
+    //first define a coroutine
+    //whatever changes the color or the sound is the coroutine
+    //coroutine is on the block
+    //define coroutine private IEnumator
+        
+    //pass the next block in (through a list?)
+    //identity the next block in the coroutine parameters
+    //if whatever that peramator is is not null and if what is playing is false
+        
+    //one script for coroutine, one script for the function, the coroutine calls the function
+}
